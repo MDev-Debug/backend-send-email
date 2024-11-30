78c470e8d75251ff5b9c2e280b584b15d8f8d374
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2024-11-30 10:37:14 -0300

    changes

---
+++ src/TestProject1/AccountServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using SendEmail.Domain.Enum;
+using SendEmail.Domain.Interfaces.Repositories;
+using SendEmail.Domain.Interfaces.Services;
+using SendEmail.Domain.Model;
+using SendEmail.Domain.Services;
+
+namespace TestProject1;
+public class AccountServiceTests
+{
+    private readonly Mock<IAccountRepository> _accountRepositoryMock;
+    private readonly Mock<ICorrelationId> _correlationIdMock;
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<ITokenService> _tokenServiceMock;
+    private readonly AccountService _accountService;
+
+    public AccountServiceTests()
+    {
+        _accountRepositoryMock = new Mock<IAccountRepository>();
+        _correlationIdMock = new Mock<ICorrelationId>();
+        _mapperMock = new Mock<IMapper>();
+        _tokenServiceMock = new Mock<ITokenService>();
+
+        _accountService = new AccountService(
+            _accountRepositoryMock.Object,
+            _correlationIdMock.Object,
+            _mapperMock.Object,
+            _tokenServiceMock.Object
+        );
+    }
+
+    [Fact]
+    public async Task AtualizarStatusConta_ShouldReturnSuccessResponse_WhenAccountExists()
+    {
+        var conta = new Account("12345678912", "Joao", "teste@gmail.com", "123@3..aBc", "11985869974");
+        var identificador = Guid.NewGuid().ToString();
+
+        _accountRepositoryMock
+            .Setup(x => x.BuscarContaPorIdentificador(identificador))
+            .ReturnsAsync(conta);
+
+        _accountRepositoryMock.Setup(x => x.AtualizarStatusConta(identificador, true));
+        var response = await _accountService.AtualizarStatusConta(identificador, true);
+
+        _accountRepositoryMock.Verify(x => x.BuscarContaPorIdentificador(identificador), Times.Once);
+        _accountRepositoryMock.Verify(x => x.AtualizarStatusConta(identificador, true), Times.Once);
+        Assert.NotNull(response);
+        response.Status.Should().Be(EResponse.OK);
+    }
+
+    [Fact]
+    public async Task AtualizarStatusConta_ShouldReturnSuccessResponse_WhenAccountNotExists()
+    {
+        var identificador = Guid.NewGuid().ToString();
+
+        _accountRepositoryMock
+            .Setup(x => x.BuscarContaPorIdentificador(identificador));
+
+        var response = await _accountService.AtualizarStatusConta(identificador, true);
+
+        _accountRepositoryMock.Verify(x => x.BuscarContaPorIdentificador(identificador), Times.Once);
+        _accountRepositoryMock.Verify(x => x.AtualizarStatusConta(identificador, true), Times.Never);
+        Assert.NotNull(response);
+        response.Status.Should().Be(EResponse.BAD_REQUEST);
+    }
+}
---
+++ src/TestProject1/TokenServiiceTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using SendEmail.Domain.Services;
+using Xunit;
+
+public class TokenServiceTests
+{
+    [Fact]
+    public async Task GenerateToken_ShouldReturnValidJwtToken_ForAdminUser()
+    {
+        // Arrange
+        var documento = "123456789";
+        var role = "admin";
+        var secretKey = "htr1rtj8tr185181*/*966jtrjrtjrtjrt"; // Deve ter no mínimo 16 caracteres
+        var audience = "https://meuapp.com";
+        var issuer = "https://issuer.com";
+
+        Environment.SetEnvironmentVariable("SECRET_KEY", secretKey);
+        Environment.SetEnvironmentVariable("Audience", audience);
+        Environment.SetEnvironmentVariable("Issuer", issuer);
+
+        var tokenService = new TokenService();
+
+        // Act
+        var token = await tokenService.GenerateToken(documento, true);
+
+        // Assert
+        ValidateToken(token, documento, role, secretKey, issuer, audience);
+    }
+
+    private void ValidateToken(string token, string documento, string role, string secretKey, string issuer, string audience)
+    {
+        Assert.NotNull(token);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var validationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+
+        var principal = tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+
+        Assert.NotNull(validatedToken);
+        Assert.IsType<JwtSecurityToken>(validatedToken);
+
+        var jwtToken = (JwtSecurityToken)validatedToken;
+        Assert.Equal(documento, principal.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        Assert.Equal(role, principal.FindFirst(ClaimTypes.Role)?.Value);
+    }
+}
